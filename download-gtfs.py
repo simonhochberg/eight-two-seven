1c77d383c84c9b4afcba416ad5ee50bd0cb1f65b
Simon Hochberg <simon.hochberg@gmail.com>
2018-09-13 19:46:21 -0700

    Create download-gtfs.py
    Added code used to access

---
+++ download-gtfs.py
@@ -0,0 +1,49 @@
+import pandas as pd, numpy as np, csv, requests, datetime, time, zipfile
+from scipy import stats
+import os, shutil
+
+deleteGTFS = input('Do you want to delete existing GTFS data? Y/N ')
+if deleteGTFS.upper() == 'Y':
+    shutil.rmtree('gtfs/')
+else:
+    pass
+    
+# download list of agency IDs from transit feeds
+
+listOfAgencyIDs = []
+api_key = "94413ac9-aec7-4720-a731-640e98d65763"
+r_url = 'https://api.transitfeeds.com/v1/getFeeds?key=%s&location=67&descendants=1&limit=100' % api_key
+r = requests.get(r_url)
+
+numPages = r.json()['results']['numPages'] # capture all gtfs feeds 
+
+for page in range(1,(numPages+1)):
+    url = r_url + "&page=%s" % str(page) # pass page argument to API url
+    r = requests.get(url)
+    for feed in (r.json()['results']['feeds']):
+        if feed['ty'] == 'gtfs' and feed['id'] not in listOfAgencyIDs:
+            listOfAgencyIDs.append(feed['id']) # append GTFS feed to list of desired feeds
+ 
+# download GTFS for each agency
+for feed in listOfAgencyIDs:
+    url = 'https://api.transitfeeds.com/v1/getLatestFeedVersion?key=%s&feed=%s' % (api_key, feed)
+    g = requests.get(url)
+    slashRemoval = '--'.join(feed.split('/'))
+    filename = '%s.zip' % slashRemoval
+    with open(filename, 'wb') as fd:
+        for chunk in g.iter_content(chunk_size=128):
+            fd.write(chunk)
+            
+# unzip downloaded GTFS data
+for feed in listOfAgencyIDs:
+    slashRemoval = '--'.join(feed.split('/'))
+    path = "%s.zip" % slashRemoval
+    new_path = 'gtfs/%s' % slashRemoval
+    try:
+        zip_ref = zipfile.ZipFile(path, 'r')
+        zip_ref.extractall(new_path)
+        zip_ref.close()
+        os.remove(path)
+    except:
+        print(path)
+        os.remove(path)
